5de900a8b91a04c6cb2f3df2fe1bc109c069244b
Disha Pant <64821756+dishap22@users.noreply.github.com>
2024-04-08 08:20:41 +0530

    Graphs

--- graphs/bfs.h
+++ graphs/bfs.h
@@ -26,3 +26,32 @@ void BFS(struct Graph* graph, int vertex)
     free(q -> data);
     free(q);
 }
+
+/*
+Taking graph as 2D Adjacency Matrix rather than struct & 0th row, column of graph as empty, as no 0 node, i.e. total n + 1 rows/cols for n node graph
+
+void BFS(int G[][7], int start, int n)
+{
+    int i = start, j;
+    queue* q = create_queue(n);
+    int visited[7] = {0};
+
+    printf("%d ", i);
+    visited[i] = 1;
+    enqueue(q, i);
+
+    while(!is_empty(q)) {
+        i = dequeue(q);
+        for (j = i; i < n; j++)
+        {
+            if (G[i][j] && !visited[j])
+            {
+                printf("%d ", j);
+                visited[j] = 1;
+                enqueue(q, j);
+            }
+        }
+    }
+}
+
+*/
--- graphs/dfs.h
+++ graphs/dfs.h
@@ -13,3 +13,27 @@ void DFS(struct Graph* graph, int vertex, int visited[])
         }
     }
 }
+
+
+/*
+Taking graph as 2D Adjacency Matrix rather than struct & 0th row, column of graph as empty, as no 0 node, i.e. total n + 1 rows/cols for n node graph
+
+void DFS(int G[][7], int start, int n)
+{
+    static int visited[7] = {0};
+
+    if (visited[start] == 0)
+    {
+        printf("%d ", start);
+        visited[start] = 1;
+        for (j = 1; j < n; j++)
+        {
+            if (G[start][j] == 1 && visited[j] == 0)
+            {
+                DFS(G, j, n);
+            }
+        }
+    }
+}
+
+*/
